2e321ab17b4dc5647adf13917998524324b52588
Fraxcelsior <marten_bonnema@hotmail.com>
2019-05-29 15:33:29 +0200

    Added js code to the detail-page to gather data of input fields, create elements and adjsut them so they display the submitted data.

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -2,23 +2,37 @@
 /*
 A user can leave their name and write a comment, when they click submit, 
 their name and comment will be displayed on the page.
---checklist:
+--to-do list:
 - X Create input field for name, one for comment, and a submit-button.
-- Add an event (name and comment) listener/handler to the button.
-- Gather data of input fields.
-- Create elements to display the data.
-- Adjust the elements so they display the data.
+- X Add an event (name and comment) listener/handler to the button.
+- X Gather data of input fields.
+- X Create elements to display the data.
+- X Adjust the elements so they display the data.
 - Display the adjusted elements on the page.
 - Empty the input fields after submitting so the next user can type new text.
 */
 
 function submitComment() {
-    console.log('I got pressed!');
-    //const formListener = document.getElementsByTagName("form");
-    //formListener.onsubmit
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    console.log(name);
+    const msgField =document.getElementById("msg");
+    const msg = msgField.value;
+    console.log(msg);
+
+    //Here we create elements for the comments
+    const comment = document.createElement("section");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    
+    
+    //Adjust the elements.
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = `${msg}`;
+    comment.classList.add("comment");
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
+
 }
-
-
-
-
 
