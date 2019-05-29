125ae267d0cf07535429f954d1966dd816b0aaa0
Fraxcelsior <marten_bonnema@hotmail.com>
2019-05-29 14:31:49 +0200

    added a javascript file to detail page with function body for the submit-form.

---
+++ pages/detail-page.js
@@ -0,0 +1,24 @@
+//detail-page.js
+/*
+A user can leave their name and write a comment, when they click submit, 
+their name and comment will be displayed on the page.
+--checklist:
+- X Create input field for name, one for comment, and a submit-button.
+- Add an event (name and comment) listener/handler to the button.
+- Gather data of input fields.
+- Create elements to display the data.
+- Adjust the elements so they display the data.
+- Display the adjusted elements on the page.
+- Empty the input fields after submitting so the next user can type new text.
+*/
+
+function submitComment() {
+    console.log('I got pressed!');
+    //const formListener = document.getElementsByTagName("form");
+    //formListener.onsubmit
+}
+
+
+
+
+
